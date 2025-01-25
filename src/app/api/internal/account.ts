67772d9eb6e0a5fa3953656bebbf744a7bbd84c5
Griseld <development@venueboost.io>
2025-01-26 01:44:59 +0200

    fix: route movement

--- src/app/api/internal/account.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '@/lib/prisma';
-
-export const accountApi = {
- getAccounts: async (clientId: string) => {
-   return prisma.account.findMany({
-     where: { clientId }
-   });
- }
-};
--- src/app/api/internal/user.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '@/lib/prisma';
-
-export const userApi = {
- getUsers: async (clientId: string) => {
-   return prisma.user.findMany({
-     where: { clientId }
-   });
- }
-};
